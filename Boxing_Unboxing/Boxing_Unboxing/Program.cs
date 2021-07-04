495e20a7dc3b21e6f37ab551639e9cd8ea46ceb5
arodriguez2323 <rodriguezarnaldo81@gmail.com>
2021-07-04 12:00:50 -0400

    First commit - c# boxing and unboxing
    First commit - c# boxing and unboxing

---
+++ Boxing_Unboxing/Boxing_Unboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing
+            int i = 123;
+            object box = i;
+
+
+            //Unboxing
+
+            object unbox = 12;
+            int z = (int)box;
+        }
+    }
+}
